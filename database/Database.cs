25ee69f950b01b0689bc6b95f4b19e5068eeabef
luizkikinho <luiz.caetano2@fatec.sp.gov.br>
2025-05-15 20:49:21 -0300

    Deletado um arquivo que manipulava a database sendo que tem outro já desempenhando essa função

--- database/Database.cs
+++ database/Database.cs
@@ -5,7 +5,35 @@ namespace Buscar.Database
 {
     public class Database
     {
+        private string connectionString = "Host=localhost;Username=admin;Password=poocarol123;Database=BusCarDB";
         private readonly string _connectionString;
+
+        public Initialize() {
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var checkCmd = conn.CreateCommand();
+            checkCmd.CommandText = @"
+                SELECT EXISTS (
+                    SELECT FROM information_schema.tables 
+                    WHERE table_schema = 'public' 
+                    AND table_name = 'users'
+                );";
+
+            bool exists = (bool)checkCmd.ExecuteScalar();
+
+            if (!exists)
+            {
+                Console.WriteLine("Tabela 'users' não existe, criando...");
+                using var createCmd = conn.CreateCommand();
+                createCmd.CommandText = File.ReadAllText("scripts/create_tables.sql");
+                createCmd.ExecuteNonQuery();
+                Console.WriteLine("Tabelas criadas com sucesso!");
+            }
+            else {
+                Console.WriteLine("Tabelas já existem");
+            }
+        }
 
         public Database(string connectionString)
         {
